85d0aa7d014a43c8b8f97dfc911f59041a6f9504
Szabó Csaba <szabo.csaba.1981@gmail.com>
2021-04-02 18:47:49 +0200

    colors is match

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,4 +1,4 @@
-const colorsArr = [
+const OPTIONSOFCOLORS = [
 	"red",
 	"grey",
 	"orange",
@@ -9,52 +9,61 @@ const colorsArr = [
 	"purple",
 	"cyan",
 ];
-let options = document.getElementsByClassName("js-choose");
-let pickedColor = [];
-let secret = [];
-let guess = [];
-let column = 1;
-let row = 1;
+let classOfOptions = document.getElementsByClassName("js-choose"); //Kiválasztható szinek osztálya
+let guess = []; // Játékos által választott színek sorszáma, max 4 darab
+let secret = []; // Random 4 jegyű titkos kód, ami nem ismétlődik
+let attempt = 1; // Lehetőségek száma
+let pickedColor = []; //A játékos által választott szín
+
+/*
+ *	Kirajzolja a játékteret
+ *
+ *	@return undefined
+ */
 function drawBoard() {
 	let divArr = []; // Lehetséges választások megjelenítésére szolgáló tömb.
 	let pickColorsArr = []; // A választható szinek megjelenítésére szolgáló tömb
-	let smallContanierArr = []; //Lehetséges találatok és színek megjelenítésére szolgáló tömb.
-	let smallItemArr = [];
+
+	let smallItemArr = []; // A segítség megjelenítsére szolgáló tömb
 	for (let i = 10; i >= 1; i--) {
-		//	smallContanierArr.push(`<div class="small-container  row${i}"></div>`); // A találatok befogadására alkalmas mező kirajzolására való függvény
 		for (let j = 0; j <= 3; j++) {
 			divArr.push(`<div class="field__item  row${i}column${j}"></div>`);
 			smallItemArr.push(`<div class="small__item  column${j}row${i}"></div>`); //Tippek megjelenítésére szolgáló mező legenerálása
 		}
 	}
-	for (let pick of colorsArr) {
+
+	/*
+	 **	Legenerálja a színválasztékot html formátumban
+	 */
+
+	for (let pick of OPTIONSOFCOLORS) {
 		pickColorsArr.push(
 			`<div data-which="${pick}" class="js-choose  field__item  ${pick}"></div>`
-		); // Lehetséges szinek választására szolgáló mező legenerálása
+		);
 	}
-
-	/*	for (let k = 0; k <= 3; k++) {
-		smallItemArr.push(`<div class="small__item  column${k}"></div>`); // Találatok megjelenítésére szolgáló mezők legenerálása
-	}*/
-
-	// A legenerált tömbök megjelenítése
+	/*
+	 * A legenerált tömbök megjelenítése
+	 */
 	document.querySelector(".field").innerHTML = divArr.join("");
 	document.querySelector(".attempt").innerHTML = smallItemArr.join("");
-	let cont = [...document.querySelectorAll(".small-container")];
-	for (var i = 0; i < cont.length; i++) {
-		cont[i].innerHTML = smallItemArr.join("");
-	}
-
 	document.querySelector(".pick").innerHTML = pickColorsArr.join("");
-	//document.querySelector(".js-start").addEventListener("click", gameLoop);
 }
 
 /* Titkos kód generálása */
-
+/**
+ * Legenerál egy '1' és '9' közötti számjegyet
+ *
+ * @return string
+ */
 function generateDigit() {
 	return String(Math.trunc(Math.random() * 9.0) + 1);
 }
 
+/**
+ * Legenerálja a négyjegyű titkos megoldást.
+ *
+ * @return Tömb '1' és '9'közt - A négy elemű nem ismétlődő titkos megoldás
+ */
 function generateSecret() {
 	let result = [];
 	while (result.length < 4) {
@@ -67,50 +76,72 @@ function generateSecret() {
 	return result;
 }
 
+/**
+ * Színválasztékot figyeli, melyik színre kattintunk.
+ *
+ * meghívja a getPickedColor fv-t
+ *
+ *@return undefined
+ */
 function eventListener() {
-	for (let option of options) {
+	for (let option of classOfOptions) {
 		option.addEventListener("click", getPickedColor);
 	}
 }
-
+/**
+ * A kiválasztott színt átadja a játkos tippjébe és kirajzolja azt.
+ *
+ * Bemeneti értéke az eventListener által átadott MouseEvent
+ *
+ * meghívja a kirajzolófv és az összehasonlító fv-kett és a segítséget kirajzoló fv-ket
+ *
+ * @return undefined
+ */
 function getPickedColor(picked) {
 	let colorMatch;
 	let colorHint;
-	pickedColor.push(picked.currentTarget.dataset.which);
-	guess.push(colorsArr.indexOf(picked.currentTarget.dataset.which) + 1);
 
-	console.log(guess);
-	drawPickedColor(pickedColor, row);
+	pickedColor.push(picked.currentTarget.dataset.which);
+	guess.push(OPTIONSOFCOLORS.indexOf(picked.currentTarget.dataset.which) + 1);
+	drawPickedColor(pickedColor, attempt);
+
 	if (guess.length === 4) {
-		colorMatch = colorsComparison(guess, secret);
-		console.log(colorMatch);
-		drawBlack(colorMatch, row);
+		colorMatch = getBlackCount(guess, secret);
+		drawBlack(colorMatch, attempt);
 		colorHint = WhiteCount(guess, secret);
-		drawWhite(colorHint, row, colorMatch);
+		drawWhite(colorHint, attempt, colorMatch);
 		guess = [];
 		pickedColor = [];
-		row += 1;
+		attempt += 1;
 	}
-	column += 1;
 }
 
-function drawPickedColor(pickedColor, row) {
-	console.log("row", row);
-	console.log("szinek", pickedColor);
-
+/**
+ * Kirajzolja a tippet a képernyőre
+ *
+ * Bemeneti értékei: Object, a kiválasztott szín és a lehetőségek száma
+ *
+ * @return undefined
+ */
+function drawPickedColor(pickedColor, attempt) {
 	for (let elem in pickedColor) {
 		document
-			.querySelector(`.row${row}column${elem}`)
+			.querySelector(`.row${attempt}column${elem}`)
 			.classList.add(`${pickedColor[elem]}`);
 	}
 }
-
-function colorsComparison(guess, secret) {
+/**
+ * Összehasonlítja a játékos tippjét és a titkós kódot, és megadja azt, hogy van-e pontos találat
+ *
+ * Bemeneti értékei: Játékos tippje Object, Titkos kód tömb
+ *
+ * @return Number, találatok száma
+ */
+function getBlackCount(guess, secret) {
 	let guessCode = guess.join("").split("");
 	let match = 0;
 
-	console.log("color1:", secret);
-	console.log("color2:", guessCode);
+	console.log("Játékos tippje:", guessCode);
 
 	for (let i = 0; i < guessCode.length; ++i) {
 		if (guessCode[i] === secret[i]) {
@@ -122,10 +153,16 @@ function colorsComparison(guess, secret) {
 	} else {
 		console.log(`${match} találat a helyén van`);
 	}
-	return match;
-	//	drawBlack(match, row);
-}
 
+	return match;
+}
+/**
+ * Összehasonlítja a játékos tippjét és a titkós kódot, és megadja azt, hogy van-e egyezés , ami nem jó helyen van
+ *
+ * Bemeneti értékei: Játékos tippje Object, Titkos kód tömb
+ *
+ * @return Number, találatok száma
+ */
 function WhiteCount(guess, secret) {
 	let found = 0;
 	let guessCopy = guess.join("").split("");
@@ -146,35 +183,50 @@ function WhiteCount(guess, secret) {
 		}
 	}
 	if (found === 0) {
-		console.log("Nincs találat");
+		console.log("Nincs egyezés");
 	} else {
-		console.log(`${found} találat nincs a helyén!`);
+		console.log(`${found} egyezés nincs a helyén!`);
 	}
 	return found;
-	//drawWhite(found, row, match);
 }
-
-function drawBlack(match, row) {
-	console.log("match", match);
-
+/**
+ * Kirajzolja a telitalálatokat
+ *
+ * Bemeneti értékei: Találat, lehetőségek száma
+ *
+ * @return undefined
+ */
+function drawBlack(match, attempt) {
 	for (let success = 0; success < match; success++) {
-		document.querySelector(`.column${success}row${row}`).classList.add(`black`);
+		document
+			.querySelector(`.column${success}row${attempt}`)
+			.classList.add(`black`);
 	}
 }
+/**
+ * Kirajzolja a színegyezést, amik nem jó helyen vannak
+ *
+ * Bemeneti értékei: egyezés, találat, lehetőségek száma
+ *
+ * @return undefined
+ */
 function drawWhite(found, row, match) {
-	console.log("hint", found);
-	console.log("row", row);
-	console.log("match", match);
-
 	for (let hint = 0; hint < found; hint++) {
 		document
-			.querySelector(`.column${hint + match}row${row}`)
+			.querySelector(`.column${hint + match}row${attempt}`)
 			.classList.add(`white`);
 	}
 }
-
+/*
+ *	Indítás
+ *
+ */
 function gameLoop() {
 	drawBoard();
+	guess = [];
+	secret = [];
+	attempt = 1;
+	pickedColor = [];
 	secret = generateSecret();
 	console.log("secretCode ", secret);
 	eventListener();
